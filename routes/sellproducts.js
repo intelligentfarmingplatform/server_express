b9f6f5872868da83147c8c3b2f8ceea11561c442
intelligentfarmingplatform <71752993+intelligentfarmingplatform@users.noreply.github.com>
2020-11-04 14:13:49 +0700

    update

--- routes/sellproducts.js
+++ routes/sellproducts.js
@@ -4,9 +4,9 @@ const UserController = require("../controllers/sellproducts");
 const { protect } = require("../utils/auth");
 
 router.use("/show" ,UserController.showproduct);
+router.put("/img/:id", UserController.img);
 router.use(protect);
 router.get("/", UserController.findAll);
-router.put("/img/:id", UserController.img);
 router.get("/:id", UserController.findOne);
 router.delete("/img/:id", UserController.deleteimg);
 router.post("/", UserController.create);
